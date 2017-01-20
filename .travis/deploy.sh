207c085dafb05d4f844864ef3c2ee36e31ab9bd7
Mateusz Bajorek <mateusz.bajorek@gmail.com>
2017-01-20 09:22:45 +0100

    simplify deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,36 +5,20 @@ if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG
-
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-
-    source .travis/gpg.sh
-
-    mvn clean deploy --settings .travis/mvnsettings.xml -DskipTests=true --batch-mode --update-snapshots
-
-
-    if [ ! -z "$TRAVIS" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
+fi
 
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
+if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+    shred -v ~/.gnupg/*
+    rm -rf ~/.gnupg
+fi
 
-    source .travis/gpg.sh
+source .travis/gpg.sh
 
-    mvn clean deploy --settings .travis/mvnsettings.xml -DskipTests=true --batch-mode --update-snapshots
+mvn clean deploy --settings .travis/mvnsettings.xml -DskipTests=true --batch-mode --update-snapshots
 
-
-    if [ ! -z "$TRAVIS" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
+if [ ! -z "$TRAVIS" ]; then
+    shred -v ~/.gnupg/*
+    rm -rf ~/.gnupg
 fi
