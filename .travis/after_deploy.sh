6ec9d7799143e98a571ae040c9b3e9402cb8b75e
Mateusz Bajorek <mateusz.bajorek@gmail.com>
2017-01-23 12:14:39 +0100

    another try at pushing to github from travis

--- .travis/after_deploy.sh
+++ .travis/after_deploy.sh
@@ -21,25 +21,31 @@ increment_version_to_next_snapshot ()
 # increment version to the next snapshot version, commit and push to repository
 if [ ! -z "$TRAVIS_TAG" ]
 then
+    # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+    openssl aes-256-cbc -K $encrypted_541ac7960f0d_key -iv $encrypted_541ac7960f0d_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
+    chmod 600 .travis/deploy_key
+    eval `ssh-agent -s`
+    ssh-add .travis/deploy_key
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+
+    # increment version to the next snapshot version
     increment_version_to_next_snapshot $TRAVIS_TAG
     echo "on a tag -> set pom.xml <version> to next SNAPSHOT $new_snapshot"
-    git remote add upstream git@github.com:CurrencyFair/OneSignal-Java-SDK.git
-    git fetch --all
-    git checkout master
     mvn --settings .travis/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$new_snapshot
     mvn --settings .travis/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.3:commit
+
     # Save some useful information
-    git config user.name "Travis CI"
-    git config user.email "build@travis-ci.org"
+    REPO=$(git config remote.origin.url)
+    REPO=${REPO/https:\/\/github.com\//git@github.com:}
+    REPO_NAME=$(basename $REPO)
+    TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
+    git clone --branch master ${REPO} ${TARGET_DIR}
+    yes | cp -f pom.xml $TARGET_DIR/
+    cd $TARGET_DIR
+    # Finally add changed pom.xml and commit
     git add pom.xml
     git commit -m "[skip ci] [Travis-CI] next snapshot version via Travis build $TRAVIS_BUILD_NUMBER"
-
-    # Get the deploy key by using Travis's stored variables to decrypt deploy_rsa.enc
-    openssl aes-256-cbc -K $encrypted_7a1322f4b03c_key -iv $encrypted_7a1322f4b03c_iv -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d
-    chmod 600 .travis/deploy_rsa
-    eval `ssh-agent -s`
-    ssh-add .travis/deploy_rsa
-
-    # Now that we're all set up, we can push.
-    git push upstream master
+    # Now that we're all set up, we can push
+    git push $REPO master
 fi
