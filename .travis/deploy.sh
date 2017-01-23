7f86de773209867af8861024026690d4376df618
Mateusz Bajorek <mateusz.bajorek@gmail.com>
2017-01-23 09:03:35 +0100

    another try at pushing to github

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -51,11 +51,28 @@ then
     echo "on a tag -> set pom.xml <version> to next SNAPSHOT $new_snapshot"
     mvn --settings .travis/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$new_snapshot
     mvn --settings .travis/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.3:commit
+    # Save some useful information
+    REPO=`git config remote.origin.url`
+    SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+    git clone $REPO out
+    # Clean out existing contents
+    cd out
     git config user.name "Travis CI"
     git config user.email "build@travis-ci.org"
-    git remote add upstream "https://$GITHUB_API_TOKEN@github.com/CurrencyFair/OneSignal-Java-SDK.git"
-    git fetch upstream
+    yes | cp -f ../pom.xml pom.xml
     git add pom.xml
     git commit -m "[Travis-CI] next snapshot version"
-    git push --quiet upstream master > /dev/null 2>&1
+
+    # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in .travis/deploy_key.enc -out .travis/deploy_key -d
+    chmod 600 .travis/deploy_key
+    eval `ssh-agent -s`
+    ssh-add .travis/deploy_key
+
+    # Now that we're all set up, we can push.
+    git push $SSH_REPO $TARGET_BRANCH
 fi
